fa711dae7378956e014e2efb923f5e50f0232e2e
Anthony Eden <anthonyeden@gmail.com>
2011-09-18 21:26:12 +0200

    Move the contact set spec to OpenSRS, which is where it belongs.

--- spec/registrar/provider/opensrs/contact_set_spec.rb
+++ spec/registrar/provider/opensrs/contact_set_spec.rb
@@ -20,7 +20,7 @@ describe Registrar::Provider::OpenSRS::ContactSet do
     it "produces xml" do
       builder = Builder::XmlMarkup.new
       contact_set = Registrar::Provider::OpenSRS::ContactSet.new(opensrs_contacts)
-      contact_set.to_xml(builder).should eq('<dt_assoc><item key=\"12345\"><dt_assoc><item key=\"first_name\">John</item><item key=\"last_name\">Doe</item><item key=\"phone\">321 213 3656</item><item key=\"fax\"></item><item key=\"email\">john.doe@example.com</item><item key=\"org_name\"></item><item key=\"address1\">1 SW 1st Street</item><item key=\"address2\">Apt 305</item><item key=\"city\">Miami</item><item key=\"state\">Florida</item><item key=\"country\">US</item><item key=\"postal_code\">33143</item></dt_assoc></item></dt_assoc>')
+      contact_set.to_xml(builder).should eq("<dt_assoc><item key=\"12345\"><dt_assoc><item key=\"first_name\">John</item><item key=\"last_name\">Doe</item><item key=\"phone\">321 213 3656</item><item key=\"fax\"></item><item key=\"email\">john.doe@example.com</item><item key=\"org_name\"></item><item key=\"address1\">1 SW 1st Street</item><item key=\"address2\">Apt 305</item><item key=\"city\">Miami</item><item key=\"state\">Florida</item><item key=\"country\">US</item><item key=\"postal_code\">33143</item></dt_assoc></item></dt_assoc>")
     end
   end
 
